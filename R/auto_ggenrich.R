41849ec045dfe81a3ee443994195db837d0c6e83
Hide-Fun <58735730+Hide-Fun@users.noreply.github.com>
2020-11-01 15:25:19 +0900

    fix problem in aspect.ratio

--- R/auto_ggenrich.R
+++ R/auto_ggenrich.R
@@ -89,7 +89,7 @@ auto_ggenrich = function(
   .stroke = .2,
   .shape_val = c(16, 17, 24),
   .width = .2, .height = .2,
-  .linesize = .2,
+  .linesize = .7,
   .linetype = "dashed",
   .hjust = 0, .vjust = 0,
   .breaks,
@@ -97,46 +97,92 @@ auto_ggenrich = function(
   .auto_fix = T,
   .ratio,
   ...) {
-  rlt <- ggplot2::ggplot(.data, .mapping) +
-    ggplot2::geom_errorbar(width = .width, size = .linesize) +
-    ggplot2::geom_errorbarh(height = .height, size = .linesize) +
-    ggtext::geom_richtext(hjust = .hjust, vjust = .vjust,
-                  fill = NA, label.color = NA,
-                  size = 6, ...) +
-    ggplot2::geom_point(size = .point_size, fill = "white", stroke = .stroke) +
-    ggplot2::geom_rect(data = .data_ref, mapping = .mapping_ref,
-              fill = "white", colour = "#028760") +
-    ggplot2::geom_hline(yintercept = 0,
-               colour = "grey",
-               linetype = .linetype, size = .linesize) +
-    ggplot2::geom_vline(xintercept = 0,
-               colour = "grey",
-               linetype = .linetype, size = .linesize)  +
-    ggplot2::scale_x_continuous(limits = .xlim,
-                       expand = c(0, 0),
-                       breaks = scales::extended_breaks(.breaks)) +
-    ggplot2::scale_y_continuous(limits = .ylim,
-                       expand = c(0, 0),
-                       breaks = scales::extended_breaks(.breaks)) +
-    ggplot2::scale_shape_manual(values = .shape_val) +
-    ggplot2::theme_classic(base_family = .family) +
-    ggplot2::theme(legend.position = "none",
-          axis.title = ggplot2::element_text(face = "bold",
-                                    size = 20),
-          axis.text = ggplot2::element_text(face = "bold",
-                                   colour = "black",
-                                   size = 15),
-          plot.margin = ggplot2::margin(.5, 1.5, .5, .5, "cm"),
-          axis.line = ggplot2::element_line(size = .7),
-          axis.ticks = ggplot2::element_line(size = .7)) +
-    ggplot2::ylab(.ylab) +
-    ggplot2::xlab(.xlab)
   if(.auto_fix == T) {
-    stund <- abs(.ylim[[1]] - .ylim[[2]]/.xlim[[1]] - .xlim[[2]])
-    rlt <- rlt +
-      ggplot2::coord_fixed(stund*.ratio)
+    stund <- abs((.xlim[[1]] - .xlim[[2]])/(.ylim[[1]] - .ylim[[2]]))
+    if (stund > 1) {
+      rlt <- ggplot2::ggplot(.data, .mapping) +
+        ggplot2::geom_errorbar(width = .width, size = .linesize) +
+        ggplot2::geom_errorbarh(height = .height*.ratio, size = .linesize) +
+        ggtext::geom_richtext(hjust = .hjust, vjust = .vjust,
+                              fill = NA, label.color = NA,
+                              size = 6, ...) +
+        ggplot2::geom_point(size = .point_size, fill = "white", stroke = .stroke) +
+        ggplot2::geom_rect(data = .data_ref, mapping = .mapping_ref,
+                           fill = "white", colour = "#028760") +
+        ggplot2::geom_hline(yintercept = 0,
+                            colour = "grey",
+                            linetype = .linetype, size = .linesize) +
+        ggplot2::geom_vline(xintercept = 0,
+                            colour = "grey",
+                            linetype = .linetype, size = .linesize)
+    } else {
+      rlt <- ggplot2::ggplot(.data, .mapping) +
+        ggplot2::geom_errorbar(width = .width*.ratio, size = .linesize) +
+        ggplot2::geom_errorbarh(height = .height, size = .linesize) +
+        ggtext::geom_richtext(hjust = .hjust, vjust = .vjust,
+                              fill = NA, label.color = NA,
+                              size = 6, ...) +
+        ggplot2::geom_point(size = .point_size, fill = "white", stroke = .stroke) +
+        ggplot2::geom_rect(data = .data_ref, mapping = .mapping_ref,
+                           fill = "white", colour = "#028760") +
+        ggplot2::geom_hline(yintercept = 0,
+                            colour = "grey",
+                            linetype = .linetype, size = .linesize) +
+        ggplot2::geom_vline(xintercept = 0,
+                            colour = "grey",
+                            linetype = .linetype, size = .linesize)
+    }
+    rlt2 <- rlt +
+      ggplot2::scale_x_continuous(limits = .xlim,
+                                  expand = c(0, 0),
+                                  breaks = scales::extended_breaks(.breaks)) +
+      ggplot2::scale_y_continuous(limits = .ylim,
+                                  expand = c(0, 0),
+                                  breaks = scales::extended_breaks(.breaks)) +
+      ggplot2::scale_shape_manual(values = .shape_val) +
+      ggplot2::theme_classic(base_family = .family) +
+      ggplot2::theme(legend.position = "none",
+                     axis.title = ggplot2::element_text(face = "bold",
+                                                        size = 20),
+                     axis.text = ggplot2::element_text(face = "bold",
+                                                       colour = "black",
+                                                       size = 15),
+                     plot.margin = ggplot2::margin(.5, 1.5, .5, .5, "cm"),
+                     axis.line = ggplot2::element_line(size = .7),
+                     axis.ticks = ggplot2::element_line(size = .7)) +
+      ggplot2::ylab(.ylab) +
+      ggplot2::xlab(.xlab)
+  } else {
+    rlt <- ggplot2::ggplot(.data, .mapping) +
+      ggplot2::geom_errorbar(width = .width, size = .linesize) +
+      ggplot2::geom_errorbarh(height = .height, size = .linesize) +
+      ggtext::geom_richtext(hjust = .hjust, vjust = .vjust,
+                            fill = NA, label.color = NA,
+                            size = 6, ...) +
+      ggplot2::geom_point(size = .point_size, fill = "white", stroke = .stroke) +
+      ggplot2::geom_rect(data = .data_ref, mapping = .mapping_ref,
+                         fill = "white", colour = "#028760") +
+      ggplot2::geom_hline(yintercept = 0,
+                          colour = "grey",
+                          linetype = .linetype, size = .linesize) +
+      ggplot2::geom_vline(xintercept = 0,
+                          colour = "grey",
+                          linetype = .linetype, size = .linesize) +
+      ggplot2::theme_classic(base_family = .family) +
+      ggplot2::theme(legend.position = "none",
+                     axis.title = ggplot2::element_text(face = "bold",
+                                                        size = 20),
+                     axis.text = ggplot2::element_text(face = "bold",
+                                                       colour = "black",
+                                                       size = 15),
+                     plot.margin = ggplot2::margin(.5, 1.5, .5, .5, "cm"),
+                     axis.line = ggplot2::element_line(size = .7),
+                     axis.ticks = ggplot2::element_line(size = .7)) +
+      ggplot2::ylab(.ylab) +
+      ggplot2::xlab(.xlab)
   }
-  return(rlt)
+  result <- rlt2 + ggplot2::coord_fixed(stund*.ratio)
+  return(result)
 }
 
 
--- R/auto_ggiso.R
+++ R/auto_ggiso.R
@@ -69,38 +69,71 @@ auto_ggiso = function(.data,
                       .family = "Arial",
                       .auto_fix = T,
                       .ratio) {
-  rlt <- ggplot2::ggplot(.data, .mapping) +
-    ggplot2::geom_errorbar(width = .width, size = .size) +
-    ggplot2::geom_errorbarh(height = .height, size = .size) +
-    ggtext::geom_richtext(hjust = .hjust, vjust = .vjust,
-                  fill = NA, label.color = NA,
-                  size = 6) +
-    ggplot2::geom_point(size = .point_size, fill = "white", stroke = .stroke) +
+  if(.auto_fix == T) {
+    stund <- abs((.xlim[[1]] - .xlim[[2]])/(.ylim[[1]] - .ylim[[2]]))
+    if(stund > 1) {
+      rlt <- ggplot2::ggplot(.data, .mapping) +
+        ggplot2::geom_errorbar(width = .width, size = .size) +
+        ggplot2::geom_errorbarh(height = .height*.ratio, size = .size) +
+        ggtext::geom_richtext(hjust = .hjust, vjust = .vjust,
+                              fill = NA, label.color = NA,
+                              size = 6) +
+        ggplot2::geom_point(size = .point_size, fill = "white", stroke = .stroke)
+    } else {
+      rlt <- ggplot2::ggplot(.data, .mapping) +
+        ggplot2::geom_errorbar(width = .width*.ratio, size = .size) +
+        ggplot2::geom_errorbarh(height = .height, size = .size) +
+        ggtext::geom_richtext(hjust = .hjust, vjust = .vjust,
+                              fill = NA, label.color = NA,
+                              size = 6) +
+        ggplot2::geom_point(size = .point_size, fill = "white", stroke = .stroke)
+    }
+    rlt2 <- rlt +
+      ggplot2::scale_x_continuous(limits = .xlim,
+                                  expand = c(0, 0),
+                                  breaks = scales::extended_breaks(.breaks)) +
+      ggplot2::scale_y_continuous(limits = .ylim,
+                                  expand = c(0, 0),
+                                  breaks = scales::extended_breaks(.breaks)) +
+      ggplot2::scale_shape_manual(values = .shape_val) +
+      ggplot2::theme_classic(base_family = .family) +
+      ggplot2::theme(legend.position = "none",
+                     axis.title = ggplot2::element_text(
+                       face = "bold",
+                       size = 20),
+                     axis.text = ggplot2::element_text(
+                       face = "bold",
+                       colour = "black",
+                       size = 15),
+                     plot.margin = ggplot2::margin(.5, 1.5, .5, .5, "cm"),
+                     axis.line = ggplot2::element_line(size = .7),
+                     axis.ticks = ggplot2::element_line(size = .7)) +
+      ggplot2::ylab(.ylab) +
+      ggplot2::xlab(.xlab)
+  } else {
+  rlt2 <- rlt +
     ggplot2::scale_x_continuous(limits = .xlim,
-                       expand = c(0, 0),
-                       breaks = scales::extended_breaks(.breaks)) +
+                                expand = c(0, 0),
+                                breaks = scales::extended_breaks(.breaks)) +
     ggplot2::scale_y_continuous(limits = .ylim,
-                       expand = c(0, 0),
-                       breaks = scales::extended_breaks(.breaks)) +
+                              expand = c(0, 0),
+                              breaks = scales::extended_breaks(.breaks)) +
     ggplot2::scale_shape_manual(values = .shape_val) +
     ggplot2::theme_classic(base_family = .family) +
     ggplot2::theme(legend.position = "none",
-                   axis.title = ggplot2::element_text(
-                     face = "bold",
-                     size = 20),
-                   axis.text = ggplot2::element_text(
-                     face = "bold",
-                     colour = "black",
-                     size = 15),
-                   plot.margin = ggplot2::margin(.5, 1.5, .5, .5, "cm"),
-                   axis.line = ggplot2::element_line(size = .7),
-                   axis.ticks = ggplot2::element_line(size = .7)) +
-    ggplot2::ylab(.ylab) +
-    ggplot2::xlab(.xlab)
-  if(.auto_fix == T) {
-    stund <- abs(.ylim[[1]] - .ylim[[2]]/.xlim[[1]] - .xlim[[2]])
-    rlt <- rlt +
-      ggplot2::coord_fixed(stund*.ratio)
+                 axis.title = ggplot2::element_text(
+                   face = "bold",
+                   size = 20),
+                 axis.text = ggplot2::element_text(
+                   face = "bold",
+                   colour = "black",
+                   size = 15),
+                 plot.margin = ggplot2::margin(.5, 1.5, .5, .5, "cm"),
+                 axis.line = ggplot2::element_line(size = .7),
+                 axis.ticks = ggplot2::element_line(size = .7)) +
+  ggplot2::ylab(.ylab) +
+  ggplot2::xlab(.xlab)
   }
-  return(rlt)
+  result <- rlt2 + ggplot2::coord_fixed(stund*.ratio)
+  return(result)
 }
