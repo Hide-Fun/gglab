a81515a9f1b3b304f7d8cb7d1fc0b10c2870067e
Hide-Fun <58735730+Hide-Fun@users.noreply.github.com>
2020-11-02 17:24:01 +0900

    remove abs

--- R/fixlimit.R
+++ R/fixlimit.R
@@ -7,7 +7,7 @@
 #' @param .ratio aspect ratio
 #' @export
 fixlmit = function(.plot, .xlim, .ylim, .ratio) {
-  stund <- abs((.xlim[[1]] - .xlim[[2]])/(.ylim[[1]] - .ylim[[2]]))
+  stund <- (.xlim[2] - .xlim[1])/(.ylim[2] - .ylim[1])
   rlt <- .plot + coord_fixed(ratio = stund*.ratio)
   return(rlt)
 }
